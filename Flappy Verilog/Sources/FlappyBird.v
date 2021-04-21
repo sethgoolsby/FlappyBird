837252c2c8d2330ae8b00d3697d1404c5eacda94
unknown <sethgool@usc.edu>
2021-04-20 22:32:16 -0700

    changed FlappyBird.v

--- Flappy Verilog/Sources/FlappyBird.v
+++ Flappy Verilog/Sources/FlappyBird.v
@@ -1,16 +1,16 @@
-module ee354_GCD(Clk, SCEN, Reset, Start, Ack, q_I, q_Grav, q_Flap, q_UnPress, q_Lost);
+module ee354_GCD(Clk, Reset, Start, Ack, Flap_Button, q_I, q_Grav, q_Flap, q_UnPress, q_Lost);
 
 	/*  INPUTS */
-	input	Clk, SCEN, Reset, Start, Ack, Flap_Button;
+	input	Clk, Reset, Start, Ack, Flap_Button;
 	
 	output reg [9:0] YBird, XBird;		
 
 	output q_I, q_Grav, q_Flap, q_UnPress, q_Lost;
-	reg [4:0] state;	
-	assign {q_I, q_Grav, q_Flap, q_UnPress, q_Lost} = state;
+	reg [3:0] state;	
+	assign {q_I, q_Grav, q_Flap, q_UnPress} = state;
 		
 	localparam 	
-	I = 5'b00001, GRAV = 5'b00010, FLAP = 5'b00100, UNPRESS = 5'b01000, LOST = 5'b10000, UNK = 5'bXXXXX;
+	I = 4'b0001, GRAV = 4'b0010, FLAP = 4'b0100, UNPRESS = 4'b1000, UNK = 4'bXXXXX;
 	
 	// NSL AND SM
 	always @ (posedge Clk, posedge Reset)
@@ -18,72 +18,33 @@ module ee354_GCD(Clk, SCEN, Reset, Start, Ack, q_I, q_Grav, q_Flap, q_UnPress, q
 		if(Reset) 
 		  begin
 			state <= I;
-			i_count <= 8'bx;  	// ****** TODO ******
-			A <= 8'bx;		  	// complete the 3 lines
-			B <= 8'bx;
-			AB_GCD <= 8'bx;			
+			YBird <= 10'bX;
+			XBird <= 10'bX;
 		  end
 		else				// ****** TODO ****** complete several parts
 				case(state)	
 					I:
 					begin
-						// state transfers
-						if (Start) state <= SUB;
-						// data transfers
-						i_count <= 0;
-						A <= Ain;
-						B <= Bin;
-						AB_GCD <= 0;
+						YBird <= 500;
+						XBird <= 100;
+						if (Start)
+						{
+							state <= GRAV:
+						}
 					end		
-					SUB: 
-		               if (SCEN) //  This causes single-stepping the SUB state
-						begin		
-							// state transfers
-							if (A == B) state <= (i_count == 0) ? DONE : MULT;
-							// data transfers
-							if (A == B) AB_GCD <= A;		
-							if (A < B)
-							  begin
-								// swap A and B
-                A <= B;
-                B <= A;
-							  end
-							else						// if (A > B)
-							  begin	
-                  if (A[0] && B[0]) 
-                    begin
-                      A <= A-B;
-                    end
-                  if (~A[0] && ~B[0])
-                    begin
-                      i_count <= i_count+1;
-                      A <=A/2;
-                      B <=B/2;
-                    end
-                  else 
-                    begin
-                      if (~A[0]) 
-                        A <= A/2;
-                      if (~B[0])
-                        B <= B/2;
-                    end
-							  end
-						end
-					MULT:
-					  if (SCEN) // This causes single-stepping the MULT state
-						begin
-							// state transfers
-                if (i_count == 1) state <= DONE;
-							// data transfers
-                AB_GCD <= AB_GCD*2;
-                i_count <= i_count-1;
-						end
-					
-					DONE:
-						if (Ack)	state <= I;
+					GRAV:
+					begin
 						
-					default:		
-						state <= UNK;
+					end
+					FLAP:
+					begin
+						
+					end
+					UNPRESS:
+					begin
+						
+					end
+					
 				endcase
 	end
 		
